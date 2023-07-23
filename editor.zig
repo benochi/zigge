d129b05eae33fe8555dc78b99567c3efacf44549
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-07-23 09:38:30 -0500

    updates

--- editor.zig
+++ editor.zig
@@ -1,2 +1,16 @@
 //handles editor loop for construction game elements.  Should allow creation of new game directories and files and linking them together.
 //should handle building the game into an exe.  Should run tests to ensure the game is runable and handle different OSs if needed.
+const std = @import("std");
+const Input = @import("input.zig");
+const graphics = @import("graphics.zig");
+const window = @import("window.zig");
+const audio = @import("audio.zig");
+const collision = @import("collision.zig");
+const es = @import("editorSettings.zig");
+
+pub fn main() !void {
+    std.debug.print("Engine started\n", .{});
+
+    // Use something from input.zig here. For example, if input.zig defines a function called handleInput:
+    try Input.handleInput();
+}
--- input.zig
+++ input.zig
@@ -1,1 +1,20 @@
 //handle all input, from keyboard, mouse, mousewheel, strafing, zoom in out, overhead, etc in editor. right click.
+const std = @import("std");
+
+pub fn handleInput() !void {
+    // Create a buffer to hold the input
+    var buffer: [1]u8 = undefined;
+
+    // Read a single byte from standard input
+    const byte_count = try std.io.getStdIn().read(buffer[0..]);
+    if (byte_count > 0) {
+        // If we read a byte, print a message based on the input
+        switch (buffer[0]) {
+            'w' => std.debug.print("You pressed W\n", .{}),
+            'a' => std.debug.print("You pressed A\n", .{}),
+            's' => std.debug.print("You pressed S\n", .{}),
+            'd' => std.debug.print("You pressed D\n", .{}),
+            else => std.debug.print("You pressed another key\n", .{}),
+        }
+    }
+}
