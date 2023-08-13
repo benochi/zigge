21462cd24227fb40b6cc0c4ad3d29351baf99ee2
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-08-13 09:20:53 -0500

    adding libraries

--- vulkanZig/src/main.zig
+++ vulkanZig/src/main.zig
@@ -1,4 +1,27 @@
 const std = @import("std");
 const c = @import("c.zig");
 
-pub fn main() !void {}
+const Entry = struct {
+    const Self = @This();
+    const LibraryNames = switch (builtin.os.tag) {
+      .windows => & [_][]const u8{"vulkan-1.dll"},
+      .ios, .macos, .tvos, .watchos => &[_][]const u8{"libvulkan.dylib", "libvulkan.1.dylib", "libMoltenVK.dylib"},
+      else => &[_][]const u8{"libvulkan.so", "libvulkan.so.1"},
+    };
+
+    fn init() !Self{ 
+      const library = std.DynLib.open(get_library_name());
+    }
+
+    fn deinit() void {
+
+    }
+
+    fn get_library_name(){
+
+    }
+};
+
+pub fn main() !void {
+    std.DynLib.open("");
+}
