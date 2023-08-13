13c0e4945e1fe637ba397cbb451cd977d0ad647d
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-08-13 09:57:42 -0500

    45 mins

--- vulkanZig/src/main.zig
+++ vulkanZig/src/main.zig
@@ -1,27 +1,41 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const c = @import("c.zig");
 
 const Entry = struct {
     const Self = @This();
     const LibraryNames = switch (builtin.os.tag) {
-      .windows => & [_][]const u8{"vulkan-1.dll"},
-      .ios, .macos, .tvos, .watchos => &[_][]const u8{"libvulkan.dylib", "libvulkan.1.dylib", "libMoltenVK.dylib"},
-      else => &[_][]const u8{"libvulkan.so", "libvulkan.so.1"},
+        .windows => &[_][]const u8{"vulkan-1.dll"},
+        .ios, .macos, .tvos, .watchos => &[_][]const u8{ "libvulkan.dylib", "libvulkan.1.dylib", "libMoltenVK.dylib" },
+        else => &[_][]const u8{ "libvulkan.so", "libvulkan.so.1" },
     };
 
-    fn init() !Self{ 
-      const library = std.DynLib.open(get_library_name());
+    handle: std.DynLib,
+
+    fn init() !Self {
+        const library = try load_library();
+        return .{
+            .handle = library,
+        };
     }
 
-    fn deinit() void {
-
+    fn deinit(self: *Self) void {
+        self.handle.close();
     }
 
-    fn get_library_name(){
+    fn lookup(self: *Self) void {
+        self.handle.lookup(c.PFN_vkGetInstanceProcAddr);
+    }
 
+    fn load_library() !std.DynLib {
+        for (LibraryNames) |library_name| {
+            return std.DynLib.open(library_name) catch continue;
+        }
+        return error.NotFound;
     }
 };
 
 pub fn main() !void {
-    std.DynLib.open("");
+    var entry = try Entry.init();
+    defer entry.deinit();
 }
